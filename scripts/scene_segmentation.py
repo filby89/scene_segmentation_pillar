207491c7968b74ad688bc997acb7da3998a1a505
filby89 <filntisis.panagiotis@gmail.com>
2024-06-03 17:47:58 +0300

    switch device to cuda and model to x

--- scripts/scene_segmentation.py
+++ scripts/scene_segmentation.py
@@ -3,11 +3,11 @@ from ultralytics import FastSAM
 
 class SceneSegmenter:
     def __init__(self):
-        self.model = FastSAM('./FastSAM-s.pt')
+        self.model = FastSAM('./FastSAM-x.pt')
 
 
     def track(self, frame, verbose=False, persist=True):
-        results = self.model.track(frame, stream=False, show=False, conf=0.8, iou=0.2, mode="track", persist=persist, device='cpu', verbose=verbose)
+        results = self.model.track(frame, stream=False, show=False, conf=0.8, iou=0.2, mode="track", persist=persist, device='cuda', verbose=verbose)
     
         return results
     
